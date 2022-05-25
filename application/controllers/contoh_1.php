466875556358c6e29687c0404f14de8ea6774ee6
sephiasyha <103979932+sephiasyha@users.noreply.github.com>
2022-05-25 17:11:25 +0700

    Initial commit

---
+++ application/controllers/contoh_1.php
@@ -0,0 +1,13 @@
+<?php
+
+class contoh_1 extends CI_Controller {
+public function index()
+	{
+		echo "Biodata Saya<br>";
+		echo "Nama Lengkap : Berliana Syela Fajrinh<br>";
+		echo "Nim : 17190243<br>";
+		echo "Fakultas : Teknik dan Informasi<br>";
+		echo "Jurusan : Teknologi Informasi<br>";
+		echo "Jenis Kelamin : Perempuan";
+	}
+}
---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1> 
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Sephia</li>
+        <li>Nama Belakang : Asyhakaila</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan palakali</li>
+    </ul>
+    
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Cirebon</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Catur</li>
+    </ul>
+</section>
