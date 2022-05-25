9a8de7db268b0129ff91ecb8a2ebd7a7a33191c2
sephiasyha <103979932+sephiasyha@users.noreply.github.com>
2022-05-25 17:16:02 +0700

    1

--- application/controllers/contoh_1.php
+++ application/controllers/contoh_1.php
@@ -4,8 +4,8 @@ class contoh_1 extends CI_Controller {
 public function index()
 	{
 		echo "Biodata Saya<br>";
-		echo "Nama Lengkap : Berliana Syela Fajrinh<br>";
-		echo "Nim : 17190243<br>";
+		echo "Nama Lengkap : Sephia Asyhakailah<br>";
+		echo "Nim : 17190237<br>";
 		echo "Fakultas : Teknik dan Informasi<br>";
 		echo "Jurusan : Teknologi Informasi<br>";
 		echo "Jenis Kelamin : Perempuan";
